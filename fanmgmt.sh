f3862d89e1cd1fa71145c2ef3455e32c60b647e8
Fedor Sedov <fedorwww@gmail.com>
2024-11-18 03:44:42 +0300

    so much better logging levels, code formatted

--- fanmgmt.sh
+++ fanmgmt.sh
@@ -37,27 +37,31 @@ elif [[ $AUTHMETHOD == 'password' ]]; then
   TOOLS+=" sshpass"
   CONNECTION_STR="sshpass -p $PASS ssh -o KexAlgorithms=diffie-hellman-group14-sha1 -o HostKeyAlgorithms=ssh-rsa $USER@$HOST"
 else
-  echo "Invalid AUTHMETHOD in config. Aborting." >&2
+  echo "<2> Invalid AUTHMETHOD in config. Aborting." >&2
   exit 1
 fi
 
-for tool in $TOOLS
-do
+for tool in $TOOLS; do
   if ! command -v "${tool}" >/dev/null 2>&1; then
-    echo "${0##*/}: ${tool} is not installed. Aborting." >&2
+    echo "<2> ${0##*/}: ${tool} is not installed. Aborting." >&2
     exit 1
   fi
 done
 
 function send_email() {
   curl --url "smtp://$SMTP_SERVER:$SMTP_PORT" \
-       --ssl-reqd \
-       --tlsv1.2 \
-       --mail-from "$SMTP_USERNAME" \
-       --mail-rcpt "$RECIPIENT" \
-       --user "$SMTP_USERNAME:$SMTP_PASSWORD" \
-       -T <(echo -e "$@")
-  >&2 echo "<4> Email sent: $*"
+    --ssl-reqd \
+    --tlsv1.2 \
+    --mail-from "$SMTP_USERNAME" \
+    --mail-rcpt "$RECIPIENT" \
+    --user "$SMTP_USERNAME:$SMTP_PASSWORD" \
+    -T <(echo -e "$@")
+  ret=$?
+  if [[ $ret == 0 ]]; then
+    echo >&2 "<5> Email sent"
+  else
+    echo >&2 "<3> Email is not sent"
+  fi
 }
 
 function unset_flags() {
@@ -70,7 +74,7 @@ function unset_flags() {
   F5="False"
   F4="False"
   F3="False"
-  >&2 echo "<5> Flags unset"
+  echo >&2 "<7> Flags unset"
 }
 
 function set_fanspeed() {
@@ -78,7 +82,7 @@ function set_fanspeed() {
   eval "${CONNECTION_STR} 'fan p 0 max $1'" >/dev/null 2>&1 || ssh_failed="True"
   eval "${CONNECTION_STR} 'fan p 0 min $2'" >/dev/null 2>&1 || ssh_failed="True"
   if [[ $ssh_failed == "False" ]]; then
-    >&2 echo "<5> Fan speed changed to (max,min): $1,$2"
+    echo >&2 "<6> Fan speed changed to (max,min): $1,$2"
   else
     subject="[HP Server] [ALERT] Cannot connect to ILO!"
     body="Cannot connect to ILO host: $HOST\n
@@ -93,18 +97,17 @@ CPU2: $CPU2
 CPU3: $CPU3
 CPU Package: $CPUP"
     message="Subject: $subject\n\n$body"
-    >&2 echo "<3> Cannot connect to ILO host: $HOST"
+    echo >&2 "<3> Cannot connect to ILO ($HOST)"
     send_email "$message"
     return 1
   fi
 }
 
-while true;
-do
-  HDD1=$(smartctl -A /dev/disk/by-id/$HDD1_ID | grep "194 Temperature_Celsius" | grep -Eo '[0-9.]+' | sed '7q;d')
-  HDD2=$(smartctl -A /dev/disk/by-id/$HDD2_ID | grep "194 Temperature_Celsius" | grep -Eo '[0-9.]+' | sed '7q;d')
-  HDD3=$(smartctl -A /dev/disk/by-id/$HDD3_ID | grep "194 Temperature_Celsius" | grep -Eo '[0-9.]+' | sed '7q;d')
-  HDD4=$(smartctl -A /dev/disk/by-id/$HDD4_ID | grep "194 Temperature_Celsius" | grep -Eo '[0-9.]+' | sed '7q;d')
+while true; do
+  HDD1=$(smartctl -A /dev/disk/by-id/"$HDD1_ID" | grep "194 Temperature_Celsius" | grep -Eo '[0-9.]+' | sed '7q;d')
+  HDD2=$(smartctl -A /dev/disk/by-id/"$HDD2_ID" | grep "194 Temperature_Celsius" | grep -Eo '[0-9.]+' | sed '7q;d')
+  HDD3=$(smartctl -A /dev/disk/by-id/"$HDD3_ID" | grep "194 Temperature_Celsius" | grep -Eo '[0-9.]+' | sed '7q;d')
+  HDD4=$(smartctl -A /dev/disk/by-id/"$HDD4_ID" | grep "194 Temperature_Celsius" | grep -Eo '[0-9.]+' | sed '7q;d')
   # Change sensors name in case these are different for your CPU
   # these can be obtained with command: sensors
   CPU0=$(sensors -Aj coretemp-isa-0000 | jq -s '.[] | .[] | ."Core 0" | .temp2_input')
@@ -113,11 +116,14 @@ do
   CPU3=$(sensors -Aj coretemp-isa-0000 | jq -s '.[] | .[] | ."Core 3" | .temp5_input')
   CPUP=$(sensors -Aj coretemp-isa-0000 | jq -s '.[] | .[] | ."Package id 0" | .temp1_input')
 
-  >&2 echo "<5> CPU0: $CPU0 | CPU1: $CPU1 | CPU2: $CPU2 | CPU3: $CPU3 | CPU Package: $CPUP | SSD: $HDD1 | HDD2: $HDD2 | HDD3: $HDD3 | HDD4: $HDD4"
+  echo >&2 "<6> CPU0: $CPU0 | CPU1: $CPU1 | CPU2: $CPU2 | CPU3: $CPU3 | CPU Package: $CPUP | SSD: $HDD1 | HDD2: $HDD2 | HDD3: $HDD3 | HDD4: $HDD4"
 
-  if [[ $HDD1 -ge $HDD_CRITICAL_TEMP  || $HDD2 -ge $HDD_CRITICAL_TEMP  || $HDD3 -ge $HDD_CRITICAL_TEMP  || $HDD4 -ge $HDD_CRITICAL_TEMP  || $CPU0 -ge $CPU_CRITICAL_TEMP  || $CPU1 -ge $CPU_CRITICAL_TEMP  || $CPU2 -ge $CPU_CRITICAL_TEMP  || $CPU3 -ge $CPU_CRITICAL_TEMP  || $CPUP -ge $CPU_CRITICAL_TEMP ]]; then
+  if [[ $HDD1 -ge $HDD_CRITICAL_TEMP || $HDD2 -ge $HDD_CRITICAL_TEMP || $HDD3 -ge $HDD_CRITICAL_TEMP || $HDD4 -ge $HDD_CRITICAL_TEMP || $CPU0 -ge $CPU_CRITICAL_TEMP || $CPU1 -ge $CPU_CRITICAL_TEMP || $CPU2 -ge $CPU_CRITICAL_TEMP || $CPU3 -ge $CPU_CRITICAL_TEMP || $CPUP -ge $CPU_CRITICAL_TEMP ]]; then
     if [[ $CRIT_TEMP_FLAG == "False" ]]; then
-      set_fanspeed $MAX $CRIT_MIN || { sleep 60; continue; }
+      set_fanspeed "$MAX" "$CRIT_MIN" || {
+        sleep 60
+        continue
+      }
       subject="[HP Server] [ALERT] Server is boiling!"
       body="TEMPERATURES:\n
 SSD: $HDD1
@@ -151,71 +157,95 @@ FLAG: $CRIT_TEMP_FLAG"
     else
       unset_flags
       CRIT_TEMP_FLAG="True"
-      >&2 echo "<4> CRIT_TEMP_FLAG=$CRIT_TEMP_FLAG"
+      echo >&2 "<4> CRIT_TEMP_FLAG=$CRIT_TEMP_FLAG"
     fi
 
-  elif [[ $HDD1 -ge $HDD_PERC90 || $HDD2 -ge $HDD_PERC90  || $HDD3 -ge $HDD_PERC90  || $HDD4 -ge $HDD_PERC90  || $CPU0 -ge $CPU_PERC90  || $CPU1 -ge $CPU_PERC90  || $CPU2 -ge $CPU_PERC90  || $CPU3 -ge $CPU_PERC90  || $CPUP -ge $CPU_PERC90 ]]; then
+  elif [[ $HDD1 -ge $HDD_PERC90 || $HDD2 -ge $HDD_PERC90 || $HDD3 -ge $HDD_PERC90 || $HDD4 -ge $HDD_PERC90 || $CPU0 -ge $CPU_PERC90 || $CPU1 -ge $CPU_PERC90 || $CPU2 -ge $CPU_PERC90 || $CPU3 -ge $CPU_PERC90 || $CPUP -ge $CPU_PERC90 ]]; then
     if [[ $F9 == "False" ]]; then
-      set_fanspeed $MAX $PERC90_MIN || { sleep 60; continue; }
+      set_fanspeed "$MAX" "$PERC90_MIN" || {
+        sleep 60
+        continue
+      }
     fi
     unset_flags
     F9="True"
-    >&2 echo "<5> F9=$F9"
+    echo >&2 "<7> F9=$F9"
 
-  elif [[ $HDD1 -ge $HDD_PERC80 || $HDD2 -ge $HDD_PERC80  || $HDD3 -ge $HDD_PERC80  || $HDD4 -ge $HDD_PERC80  || $CPU0 -ge $CPU_PERC80  || $CPU1 -ge $CPU_PERC80  || $CPU2 -ge $CPU_PERC80  || $CPU3 -ge $CPU_PERC80  || $CPUP -ge $CPU_PERC80 ]]; then
+  elif [[ $HDD1 -ge $HDD_PERC80 || $HDD2 -ge $HDD_PERC80 || $HDD3 -ge $HDD_PERC80 || $HDD4 -ge $HDD_PERC80 || $CPU0 -ge $CPU_PERC80 || $CPU1 -ge $CPU_PERC80 || $CPU2 -ge $CPU_PERC80 || $CPU3 -ge $CPU_PERC80 || $CPUP -ge $CPU_PERC80 ]]; then
     if [[ $F8 == "False" ]]; then
-      set_fanspeed $MAX $PERC80_MIN || { sleep 60; continue; }
+      set_fanspeed "$MAX" "$PERC80_MIN" || {
+        sleep 60
+        continue
+      }
     fi
     unset_flags
     F8="True"
-    >&2 echo "<5> F8=$F8"
+    echo >&2 "<7> F8=$F8"
 
-  elif [[ $HDD1 -ge $HDD_PERC70  || $HDD2 -ge $HDD_PERC70  || $HDD3 -ge $HDD_PERC70  || $HDD4 -ge $HDD_PERC70  || $CPU0 -ge $CPU_PERC70  || $CPU1 -ge $CPU_PERC70  || $CPU2 -ge $CPU_PERC70  || $CPU3 -ge $CPU_PERC70  || $CPUP -ge $CPU_PERC70 ]]; then
+  elif [[ $HDD1 -ge $HDD_PERC70 || $HDD2 -ge $HDD_PERC70 || $HDD3 -ge $HDD_PERC70 || $HDD4 -ge $HDD_PERC70 || $CPU0 -ge $CPU_PERC70 || $CPU1 -ge $CPU_PERC70 || $CPU2 -ge $CPU_PERC70 || $CPU3 -ge $CPU_PERC70 || $CPUP -ge $CPU_PERC70 ]]; then
     if [[ $F7 == "False" ]]; then
-      set_fanspeed $MAX $PERC70_MIN || { sleep 60; continue; }
+      set_fanspeed "$MAX" "$PERC70_MIN" || {
+        sleep 60
+        continue
+      }
     fi
     unset_flags
     F7="True"
-    >&2 echo "<5> F7=$F7"
+    echo >&2 "<7> F7=$F7"
 
-  elif [[ $HDD1 -ge $HDD_PERC60  || $HDD2 -ge $HDD_PERC60  || $HDD3 -ge $HDD_PERC60  || $HDD4 -ge $HDD_PERC60  || $CPU0 -ge $CPU_PERC60  || $CPU1 -ge $CPU_PERC60  || $CPU2 -ge $CPU_PERC60  || $CPU3 -ge $CPU_PERC60  || $CPUP -ge $CPU_PERC60 ]]; then
+  elif [[ $HDD1 -ge $HDD_PERC60 || $HDD2 -ge $HDD_PERC60 || $HDD3 -ge $HDD_PERC60 || $HDD4 -ge $HDD_PERC60 || $CPU0 -ge $CPU_PERC60 || $CPU1 -ge $CPU_PERC60 || $CPU2 -ge $CPU_PERC60 || $CPU3 -ge $CPU_PERC60 || $CPUP -ge $CPU_PERC60 ]]; then
     if [[ $F6 == "False" ]]; then
-      set_fanspeed $MAX $PERC60_MIN || { sleep 60; continue; }
+      set_fanspeed "$MAX" "$PERC60_MIN" || {
+        sleep 60
+        continue
+      }
     fi
     unset_flags
     F6="True"
-    >&2 echo "<5> F6=$F6"
+    echo >&2 "<7> F6=$F6"
 
-  elif [[ $HDD1 -ge $HDD_PERC50  || $HDD2 -ge $HDD_PERC50  || $HDD3 -ge $HDD_PERC50  || $HDD4 -ge $HDD_PERC50  || $CPU0 -ge $CPU_PERC50  || $CPU1 -ge $CPU_PERC50  || $CPU2 -ge $CPU_PERC50  || $CPU3 -ge $CPU_PERC50  || $CPUP -ge $CPU_PERC50 ]]; then
+  elif [[ $HDD1 -ge $HDD_PERC50 || $HDD2 -ge $HDD_PERC50 || $HDD3 -ge $HDD_PERC50 || $HDD4 -ge $HDD_PERC50 || $CPU0 -ge $CPU_PERC50 || $CPU1 -ge $CPU_PERC50 || $CPU2 -ge $CPU_PERC50 || $CPU3 -ge $CPU_PERC50 || $CPUP -ge $CPU_PERC50 ]]; then
     if [[ $F5 == "False" ]]; then
-      set_fanspeed $MAX $PERC50_MIN || { sleep 60; continue; }
+      set_fanspeed "$MAX" "$PERC50_MIN" || {
+        sleep 60
+        continue
+      }
     fi
     unset_flags
     F5="True"
-    >&2 echo "<5> F5=$F5"
+    echo >&2 "<7> F5=$F5"
 
-  elif [[ $HDD1 -ge $HDD_PERC40  || $HDD2 -ge $HDD_PERC40  || $HDD3 -ge $HDD_PERC40  || $HDD4 -ge $HDD_PERC40  || $CPU0 -ge $CPU_PERC40  || $CPU1 -ge $CPU_PERC40  || $CPU2 -ge $CPU_PERC40  || $CPU3 -ge $CPU_PERC40  || $CPUP -ge $CPU_PERC40 ]]; then
+  elif [[ $HDD1 -ge $HDD_PERC40 || $HDD2 -ge $HDD_PERC40 || $HDD3 -ge $HDD_PERC40 || $HDD4 -ge $HDD_PERC40 || $CPU0 -ge $CPU_PERC40 || $CPU1 -ge $CPU_PERC40 || $CPU2 -ge $CPU_PERC40 || $CPU3 -ge $CPU_PERC40 || $CPUP -ge $CPU_PERC40 ]]; then
     if [[ $F4 == "False" ]]; then
-      set_fanspeed $MAX $PERC40_MIN || { sleep 60; continue; }
+      set_fanspeed "$MAX" "$PERC40_MIN" || {
+        sleep 60
+        continue
+      }
     fi
     unset_flags
     F4="True"
-    >&2 echo "<5> F4=$F4"
+    echo >&2 "<7> F4=$F4"
 
-  elif [[ $HDD1 -gt $HDD_PERC30  || $HDD2 -gt $HDD_PERC30  || $HDD3 -gt $HDD_PERC30  || $HDD4 -gt $HDD_PERC30  || $CPU0 -gt $CPU_PERC30  || $CPU1 -gt $CPU_PERC30  || $CPU2 -gt $CPU_PERC30  || $CPU3 -gt $CPU_PERC30  || $CPUP -gt $CPU_PERC30 ]]; then
+  elif [[ $HDD1 -gt $HDD_PERC30 || $HDD2 -gt $HDD_PERC30 || $HDD3 -gt $HDD_PERC30 || $HDD4 -gt $HDD_PERC30 || $CPU0 -gt $CPU_PERC30 || $CPU1 -gt $CPU_PERC30 || $CPU2 -gt $CPU_PERC30 || $CPU3 -gt $CPU_PERC30 || $CPUP -gt $CPU_PERC30 ]]; then
     if [[ $F3 == "False" ]]; then
-      set_fanspeed $MAX $PERC30_MIN || { sleep 60; continue; }
+      set_fanspeed "$MAX" "$PERC30_MIN" || {
+        sleep 60
+        continue
+      }
     fi
     unset_flags
     F3="True"
-    >&2 echo "<5> F3=$F3"
+    echo >&2 "<7> F3=$F3"
 
   else
     if [[ $COLD_TEMP_FLAG == "False" ]]; then
-      set_fanspeed $COLD_MAX $COLD_MIN || { sleep 60; continue; }
+      set_fanspeed "$COLD_MAX" "$COLD_MIN" || {
+        sleep 60
+        continue
+      }
       unset_flags
       COLD_TEMP_FLAG="True"
-      >&2 echo "<4> COLD_TEMP_FLAG=$COLD_TEMP_FLAG"
+      echo >&2 "<4> COLD_TEMP_FLAG=$COLD_TEMP_FLAG"
       subject="[HP Server] [ALERT] Server is freezing!"
       body="TEMPERATURES:\n
 SSD: $HDD1
